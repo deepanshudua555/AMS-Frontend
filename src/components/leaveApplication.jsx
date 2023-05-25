880178cebb3dab604c46a68c82cb9e4bf88bf22f
surbhitt <surbhit.thakur@gmail.com>
2023-05-25 14:05:26 +0530

    data.reason.len>11 && data.subject.len>5

--- src/components/leaveApplication.jsx
+++ src/components/leaveApplication.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import Letter from './letter'
-import ActiveLeaves from './activeLeaves'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import React from "react";
+import Letter from "./letter";
+import ActiveLeaves from "./activeLeaves";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+
 
 export default function LeaveApplication() {
   return (
     <div>
       <div>
-        <ul className="flex justify-evenly w-1/2 mx-auto">
-          <Link to="/leave/activeleaves">
-            <li className="flex justify-center border border-black rounded-md p-2">
+          <ul className="flex justify-evenly w-1/2 mx-auto">
+            <Link to="/leave/activeleaves">
+              <li className="flex justify-center border border-black rounded-md p-2">
               Active Leaves
             </li>
-          </Link>
-          <Link to="/leave/letter">
-            <li className="flex justify-center border border-black rounded-md p-2 w-32">
+            </Link>
+            <Link to="/leave/letter">
+              <li className="flex justify-center border border-black rounded-md p-2 w-32">
               Letter
             </li>
-          </Link>
-        </ul>
+            </Link>
+          </ul>
       </div>
+    
     </div>
-  )
+  );
 }
