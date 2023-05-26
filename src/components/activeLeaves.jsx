c32f16266767e0c35cc786ab1fabf8bb35e75c36
surbhitt <surbhit.thakur@gmail.com>
2023-05-26 21:08:24 +0530

    refactored leaves bar

--- src/components/activeLeaves.jsx
+++ src/components/activeLeaves.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
 import Bar from './activeLeavesBar'
+import { useEffect } from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function ActiveLeaves() {
-  return <Bar />
+  const [leaves, setLeaves] = useState([])
+  // let leaves;
+  const getLeaves = async (event) => {
+    // event.preventDefault();
+    const token = localStorage.getItem('token')
+    let objectleaves = await axios.get(
+      'http://localhost:5000/api/leaveapplication/checkleaves',
+      {
+        headers: {
+          'auth-token': localStorage.getItem('token'),
+        },
+      }
+    )
+    setLeaves(objectleaves.data)
+    console.log(leaves)
+  }
+  useEffect(() => {
+    getLeaves()
+  }, [])
+  return (
+    <div>
+      {leaves.map((leave) => {
+        <Bar name={leave.name}/>
+      })}
+    </div>
+  )
 }
--- src/components/activeLeavesBar.jsx
+++ src/components/activeLeavesBar.jsx
@@ -1,48 +1,29 @@
-import { useEffect } from 'react';
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from "react"
 
-
-
-
-const Bar = () => {
-    const [leaves,setLeaves]=useState([]);
-    // let leaves;
-    const getLeaves=async(event)=>{
-        // event.preventDefault();
-        const token=localStorage.getItem('token')
-        let objectleaves=await axios.get('http://localhost:5000/api/leaveapplication/checkleaves',{
-            headers:{
-                "auth-token": localStorage.getItem('token')
-            }
-        })
-        setLeaves(objectleaves.data);
-        console.log(leaves);
-
-    }
-    useEffect(()=>{
-        getLeaves();
-    },[])
-    return (
-        <div>
-
-            {
-                leaves.map((leave)=>{
-                    return (
-                    <div className="flex border border-black items-center w-2/3 mx-auto my-10">
-                    <img src="/teacheravatar.png" className='w-20'/>
-                                 <span className="name">{leave.name}</span>
-                                 <ul className="flex ml-auto">
-                                     <li><button className='flex bg-blue-400 mr-4 h-8 w-28 items-center justify-center rounded-md border border-black'>View Letter</button></li>
-                                     <li><button className='flex bg-green-400 mr-4 h-8 w-20 items-center justify-center rounded-md border border-black'>Approve</button></li>
-                                     <li><button className='flex bg-red-400 mr-4 h-8 w-20 items-center justify-center rounded-md border border-black'>Reject</button></li>
-                                 </ul>
-                         </div>)
-                })
-            }
-
-        </div>  
-    )
+const Bar = (props) => {
+  return (
+    <div className="flex border border-black items-center w-2/3 mx-auto my-10">
+      <img src="/teacheravatar.png" className="w-20" />
+      <span className="name">{props.name}</span>
+      <ul className="flex ml-auto">
+        <li>
+          <button className="flex bg-blue-400 mr-4 h-8 w-28 items-center justify-center rounded-md border border-black">
+            View Letter
+          </button>
+        </li>
+        <li>
+          <button className="flex bg-green-400 mr-4 h-8 w-20 items-center justify-center rounded-md border border-black">
+            Approve
+          </button>
+        </li>
+        <li>
+          <button className="flex bg-red-400 mr-4 h-8 w-20 items-center justify-center rounded-md border border-black">
+            Reject
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Bar
