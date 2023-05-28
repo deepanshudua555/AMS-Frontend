a791b802c5f5a2c394697688f921ec251a4c36b5
surbhitt <surbhit.thakur@gmail.com>
2023-05-28 11:08:58 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import StudentLoginAndRegistration from './components/studentLoginAndRegistration';
-import TeacherLoginAndRegistration from './components/teacherLoginAndRegistration';
-import Timetable from './components/timetable';
-import Notices from './components/notices';
-import LeaveState from './Context/leaveState';
+import Navbar from './components/navbar'
+import StudentLoginAndRegistration from './components/studentLoginAndRegistration'
+import TeacherLoginAndRegistration from './components/teacherLoginAndRegistration'
+import Timetable from './components/timetable'
+import Notices from './components/notices'
+import LeaveState from './Context/leaveState'
 import Leave from './components/leaveApplication'
-import ActiveLeaves from './components/activeLeaves';
-import Letter from './components/letter';
-import { useContext } from 'react';
-import leaveContext from './Context/leaveContext';
+import ActiveLeaves from './components/activeLeaves'
+import Letter from './components/letter'
+import { useContext } from 'react'
+import leaveContext from './Context/leaveContext'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   // Link
-} from "react-router-dom";
+} from 'react-router-dom'
 
 function App() {
   // const contextData = useContext(leaveContext);
@@ -26,24 +26,29 @@ function App() {
     <>
       <LeaveState>
         <Router>
-        {/* isLoggedInStud={isLoggedInStud} isLoggedInTeach={isLoggedInTeach} */}
+          {/* isLoggedInStud={isLoggedInStud} isLoggedInTeach={isLoggedInTeach} */}
           <Navbar />
-          
+
           {/* {alert && <Alert/>} */}
           <Routes>
             <Route path="/" element={<Notices />} />
-            <Route path="/studentcreds" element={<StudentLoginAndRegistration />} />
-            <Route path="/teachercreds" element={<TeacherLoginAndRegistration />} />
+            <Route
+              path="/studentcreds"
+              element={<StudentLoginAndRegistration />}
+            />
+            <Route
+              path="/teachercreds"
+              element={<TeacherLoginAndRegistration />}
+            />
             <Route path="/leave" element={<Leave />} />
             <Route path="/timetable" element={<Timetable />} />
             {/* <Route path="/leave/activeleaves" element={<ActiveLeaves />} />
             <Route path="/leave/letter" element={<Letter />} /> */}
           </Routes>
         </Router>
-
       </LeaveState>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function Navbar(props) {
+  // localStorage.clear();
+
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+  const user = localStorage.getItem('user')
+
+  const handleLogout = () => {
+    localStorage.clear()
+    navigate('/')
+  }
+
+  const buttonInactive =
+    'bg-blue-500 hover:bg-white text-white hover:text-gray-800 py-2 px-2 border-2 hover:border-black rounded-full w-32'
+  const buttonActive =
+    'cursor-default bg-white text-black py-2 px-2 border-2 border-black rounded-full w-32'
+  return (
+    <>
+      <nav className="py-4 px-2 mx-10 items-center relative">
+        <div className="flex mx-auto mt-4">
+          <a href="https://bpitindia.com/" className="flex items-center">
+            <img
+              src="/bpit-logo.jpeg"
+              className="h-20 mr-3 rounded-full border-4 p-1"
+              alt="bpit logo"
+            />
+            <span className="text-xl font-semibold text-gray-800">
+              Bhagwan parshuram institute of Technology
+            </span>
+          </a>
+
+          <ul className="flex ml-auto w-1/2 justify-evenly items-center">
+            <li className={`${token === null ? 'hidden' : ''}`}>
+              <Link to="/">
+                <button
+                  className={props.leaveButton ? buttonActive : buttonInactive}
+                >
+                  Home
+                </button>
+              </Link>
+            </li>
+
+            <li
+              className={`${
+                token === null || user === 'student' ? 'hidden' : ''
+              }`}
+            >
+              <Link to="/leave">
+                <button
+                  className={props.leaveButton ? buttonActive : buttonInactive}
+                >
+                  Leave
+                </button>
+              </Link>
+            </li>
+
+            {/* <li className={props.attendanceButton ? buttonActive : buttonInactive} >
+                            <Link to='/attendance' >Attendance</Link>
+                        </li> */}
+
+            <li className={`${token === null ? 'hidden' : ''}`}>
+              <Link to="/timetable">
+                <button
+                  className={
+                    props.timetableButton ? buttonActive : buttonInactive
+                  }
+                >
+                  Time-Table
+                </button>
+              </Link>
+            </li>
+
+            {/* className={props.lsButton ? buttonActive : buttonInactive} */}
+            {/* <li className={`${token===null?'hidden':''}`}>
+                <Link to="/studentcreds">Student Login/Register</Link>
+              </li> */}
+            {/* className={props.lsButton ? buttonActive : buttonInactive} */}
+            {/* <li className={`${token===null?'hidden':''}`}>
+                <Link to="/teachercreds">Teacher Login/Register</Link>
+              </li> */}
+
+            {/* logout button  */}
+            <li className={`${token === null ? 'hidden' : ''}`}>
+              <button
+                onClick={handleLogout}
+                className={props.leaveButton ? buttonActive : buttonInactive}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+        <hr className="border border-gray-600 mt-4" />
+      </nav>
+    </>
+  )
+}
